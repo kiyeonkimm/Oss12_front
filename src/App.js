a335d52dad8eae5847700079a1d5f0b393c2fec1
kiyeonkimm <paynjoy2@naver.com>
2024-04-14 18:41:55 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Advertisement from "./components/Advertisement";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Advertisement />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
